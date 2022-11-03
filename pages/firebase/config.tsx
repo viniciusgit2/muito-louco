de3d6f2ee89002a01c3cf77ef0dbe99ab7fc3a14
Vinicius Patelfin <vinicius.patelfin@splendasa.com.br>
2022-11-03 10:28:42 -0300

    testar o firebase

---
+++ pages/firebase/config.tsx
@@ -0,0 +1,5 @@
+import firebase from "firebase";
+import "firebase/firestore";
+if(!firebase.apps.l){
+    
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/**
+ @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: {html,js,jsx,tsx,ts},
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
